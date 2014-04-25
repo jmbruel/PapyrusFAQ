ecfae987846c7b46fdce69e2bac19d0361074ed3
Jean-Michel Bruel <jbruel@gmail.com>
2014-04-25 16:27:20 +0200

    shell script improvements

--- update-doc.sh
+++ update-doc.sh
@@ -10,12 +10,13 @@ git config --global user.email "jbruel#travis@gmail.com"
 git config --global user.name "Jean-Michel Bruel"
 git config --global push.default simple
 
-#using token clone gh-pages branch
+#using token clone doc branch
 git clone --quiet --branch=doc https://${GH_TOKEN}@github.com/jmbruel/PapyrusFAQ.git  doc > /dev/null
 
-#go into diractory and copy data we're interested in to that directory
+#go into directory and copy data we're interested in to that directory
 cd doc
 cp -Rf $HOME/output/* ./output
+ls -al ./output
 
 #add, commit and push files without provoking Continuous Integration
 git add -f .
